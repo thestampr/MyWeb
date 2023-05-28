754dbe641ce3647931d21fed7345cb9665521ea9
stampr <thestampmc@gmail.com>
2023-05-28 19:16:51 +0700

    New navbar for mobile

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -12,17 +12,29 @@ import { AppearanceService } from '../appearance.service';
 export class TopbarComponent{
     constructor(public theme: AppearanceService, private elementRef: ElementRef, public router: Router) {}
 
-    ToogleTheme(): void {
-        this.theme.is_dark = !this.theme.is_dark;
-        this._changeTheme();
+    private _changeTheme(): void {
+        let body: HTMLElement = this.elementRef.nativeElement.ownerDocument.body;
+        body.style.backgroundColor = this.theme.background_color;
     }
 
     isActive(path: string): boolean {
         return document.location.pathname.endsWith(path);
     }
 
-    private _changeTheme(): void {
-        let body: HTMLElement = this.elementRef.nativeElement.ownerDocument.body;
-        body.style.backgroundColor = this.theme.background_color;
+    ToogleTheme(): void {
+        this.theme.is_dark = !this.theme.is_dark;
+        this._changeTheme();
+    }
+
+    OpenNav(): void {
+        let floating_nav: HTMLElement = document.getElementById("mobile-nav-bg")!;
+
+        floating_nav.classList.add("open");
+    }
+
+    CloseNav(): void {
+        let floating_nav: HTMLElement = document.getElementById("mobile-nav-bg")!;
+
+        floating_nav.classList.remove("open");
     }
 }
