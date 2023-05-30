82f59f83bb1137599accc855654ae35423d4eb97
stampr <thestampmc@gmail.com>
2023-05-30 13:18:40 +0700

    Fix topbar

--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AppearanceService } from '../appearance.service';
@@ -10,12 +10,59 @@ import { AppearanceService } from '../appearance.service';
   styleUrls: ['./topbar.component.css']
 })
 export class TopbarComponent{
+
+    private tsX: number;
+    private teX: number;
+    private tsY: number;
+    private teY: number;
     
     constructor(public theme: AppearanceService, public router: Router, private elementRef: ElementRef) {}
 
     private _changeTheme(): void {
         let body: HTMLElement = this.elementRef.nativeElement.ownerDocument.body;
         body.style.backgroundColor = this.theme.background_color;
+    }
+
+    @HostListener("window:resize")
+    OnResize(event: TouchEvent): void {
+        // console.log('Resize');
+
+        if (window.innerWidth > 900) {
+            this.CloseNav();
+        }
+    }
+
+    @HostListener("window:touchstart", ["$event"])
+    OnTouchStart(event: TouchEvent): void {
+        this.tsX = event.touches[0].clientX;
+        this.tsY = event.touches[0].clientY;
+    }
+
+    @HostListener("window:touchend", ["$event"])
+    OnTouchEnd(event: TouchEvent): void {
+        this.teX = event.changedTouches[0].clientX;
+        this.teY = event.changedTouches[0].clientY;
+
+        if ((this.tsX > this.teX+50) && this.tsX > window.innerWidth - 20) {
+            // right
+            // console.log('swipe from right');
+
+            this.OpenNav();
+
+        } else if (this.tsX < this.teX-50) {
+            // left
+            // console.log('left');
+
+            this.CloseNav();
+        }
+
+        if (this.tsY > this.teY+50) {
+            // down
+            // console.log('down');
+        } else if (this.tsY < this.teY-50) {
+            // up
+            // console.log('up');
+        }
     }
 
     isActive(path: string): boolean {
@@ -28,9 +75,11 @@ export class TopbarComponent{
     }
 
     OpenNav(): void {
-        let floating_nav: HTMLElement = document.getElementById("mobile-nav-bg")!;
-
-        floating_nav.classList.add("open");
+        if (window.innerWidth <= 900) {
+            let floating_nav: HTMLElement = document.getElementById("mobile-nav-bg")!;
+    
+            floating_nav.classList.add("open");
+        }
     }
 
     CloseNav(): void {
