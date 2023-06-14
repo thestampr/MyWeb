0b7cd0e1479d049792799ea12043046bffa5a52a
stampr <thestampmc@gmail.com>
2023-06-15 01:54:00 +0700

    Add wave background

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -19,19 +19,23 @@ export class ResumeComponent {
 
     async SaveAsPng() {
         let content: HTMLElement = document.getElementById('resume-content')!;
-        let backend = this.backend;
 
+        let backend = this.backend;
         await htmlToImage.toPng(content).then(async function (resume_img) {
             backend.Download(resume_img, "Peeradon's resume.png");
         });
     }
 
     OpenModal(): void {
+        let content: HTMLElement = document.getElementById('resume-content')!;
+        content.classList.remove("no-background");
+
         let modal = document.getElementById("download-modal")!;
         modal.classList.add("open");
 
         modal.addEventListener("click", () => {
             modal.classList.remove("open");
+            content.classList.add("no-background");
         })
     }
 }
