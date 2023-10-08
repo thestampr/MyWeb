b6b1950411361257e1a7d170bcc8f5804fb0652e
stampr <thestampmc@gmail.com>
2023-10-08 22:58:40 +0700

    Fixed: modal view not destroy

--- src/app/modal-view.directive.ts
+++ src/app/modal-view.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, HostListener, OnInit, ElementRef } from '@angular/core';
+import { Directive, Input, HostListener, OnInit, ElementRef, OnDestroy } from '@angular/core';
 
 import { BackendService } from './backend.service';
 
@@ -6,7 +6,7 @@ import { BackendService } from './backend.service';
 @Directive({
     selector: '[ModalView]'
 })
-export class ModalViewDirective implements OnInit {
+export class ModalViewDirective implements OnInit, OnDestroy {
 
     @Input() src: string = "";
     @Input() parallax: boolean;
@@ -14,10 +14,12 @@ export class ModalViewDirective implements OnInit {
 
     private _source: string;
 
-    private modal_bg: HTMLDivElement;
-    private modal_image: HTMLImageElement;
+    private modal_bg?: HTMLDivElement;
+    private modal_image?: HTMLImageElement;
 
-    constructor(private elementRef: ElementRef, private backend: BackendService) {}
+    constructor(private elementRef: ElementRef, private backend: BackendService) {
+        
+    }
 
     get element(): HTMLElement {
         return this.elementRef.nativeElement;
@@ -29,14 +31,18 @@ export class ModalViewDirective implements OnInit {
         this._createElement();
     }
 
+    ngOnDestroy(): void {
+        this._destroyElement();
+    }
+
     @HostListener('click') 
     private openMoal() {
-        this.modal_bg.classList.add("open");
+        this.modal_bg!.classList.add("open");
     }
 
     @HostListener('document:keydown', ['$event']) 
     private onEscape(event: KeyboardEvent) {
-        if (event.key === "Escape" && this.modal_bg.classList.contains("open")) {
+        if (event.key === "Escape" && this.modal_bg?.classList.contains("open")) {
             this.modal_bg.classList.remove("open");
         }
     }
@@ -50,14 +56,14 @@ export class ModalViewDirective implements OnInit {
             const x: number = (50 - event.clientX - (window.innerWidth / 2)) * self.sensitive;
             const y: number = (50 - event.clientY - (window.innerHeight / 2)) * self.sensitive;
     
-            self.modal_image.style.transform = `translateX(${x}px) translateY(${y}px)`;
+            self.modal_image!.style.transform = `translateX(${x}px) translateY(${y}px)`;
         }
     
         function handleGyroscope(event: DeviceOrientationEvent): void {
             const x: number = (50 - event.beta!) * self.sensitive;
             const y: number = (50 - event.gamma!) * self.sensitive;
         
-            self.modal_image.style.transform = `translateX(${x}px) translateY(${y}px)`;
+            self.modal_image!.style.transform = `translateX(${x}px) translateY(${y}px)`;
         }
 
         function createParallax(): void {
@@ -82,7 +88,7 @@ export class ModalViewDirective implements OnInit {
         document.body.appendChild(this.modal_bg);
 
         this.modal_bg.addEventListener("click", () => {
-            this.modal_bg.classList.remove("open");
+            this.modal_bg!.classList.remove("open");
         });
 
         this.modal_image.addEventListener("click", (event: MouseEvent) => {
@@ -93,5 +99,9 @@ export class ModalViewDirective implements OnInit {
         if (this.parallax) {
             createParallax();
         }
+    }
+
+    private _destroyElement() {
+        document.body.removeChild(this.modal_bg!);
     }
 }
